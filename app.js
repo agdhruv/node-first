39eacc905a0e8b8f2d44a8c21255deeae2d9bd10
agdhruv <dhruv_agarwal@live.com>
2016-07-07 13:12:12 +0530

    Commit after class on 7th July

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-var express = require('express');
-var app = express();
+var express11 = require('express');
+var app = express11();
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
+});
+
+app.get('/name', function (req, res) {
+  res.download("/Users/Dhruv/Pictures/DSC_0037.jpg",function(err){
+  	console.log(err);
+  });
 });
 
 app.listen(3000, function () {
