f9c19d75d6256fcaa4a6a122a46880b4ae5653d3
agdhruv <dhruv_agarwal@live.com>
2016-07-07 22:49:13 +0530

    Removed res.download() at home

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
-var express11 = require('express');
-var app = express11();
+var express = require('express');
+var app = express();
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
-});
-
-app.get('/name', function (req, res) {
-  res.download("/Users/Dhruv/Pictures/DSC_0037.jpg",function(err){
-  	console.log(err);
-  });
 });
 
 app.listen(3000, function () {
